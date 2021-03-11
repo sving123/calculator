2a0d79b675734ddc0d87fe30d7185ab832127c56
sving123 <ilya_brich@mail.ru>
2021-03-11 04:27:59 +0300

    Sborka project

--- funcs.h
+++ funcs.h
@@ -2,9 +2,8 @@
 #define __FUNCS_H__
 
 double MySum(double, double);
-double MuSub(double, double);
-double MyDiv(double, double);
 double MyMul(double, double);
-
+double MySub(double, double);
+double MyDiv(double, double);
 
 #endif // __FUNCS_H__
